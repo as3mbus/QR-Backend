98411c0445e1590354e2971d4e76e5a7231525c8
as3mbus <asembus@gmail.com>
2017-11-09 14:28:23 +0700

    arrange code for more functional calls

--- common/models/voucher-code.js
+++ common/models/voucher-code.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Vouchercode) {
-
+module.exports = function (VoucherCode) {
+  VoucherCode.activate = function (req, res) {
+    VoucherCode.findById(req.params.codeid, function (err, VoucherCode) {
+      if (err) res.json(err);
+      if (VoucherCode) {
+        VoucherCode.updateAttribute("activated", true, function (uerr, uVoucherCode) {
+          res.json(uVoucherCode);
+        })
+      } else {
+        var error = new Error();
+        error.message = 'Voucher Code not found.';
+        error.statusCode = 404;
+        res.json(error);
+      }
+    });
+  };
+  VoucherCode.isActive = function (req, res) {
+    VoucherCode.findById(req.params.codeid,
+      function (err, vCode) {
+        if (err) {
+          console.log(err);
+          res.json(err);
+        } else {
+          if (vCode) {
+            res.json(vCode.activated);
+          } else {
+            var error = new Error();
+            error.message = 'Voucher Code not found.';
+            error.statusCode = 404;
+            res.json(error);
+          }
+        }
+      }
+    );
+  };
 };
--- server/boot/activate.js
+++ server/boot/activate.js
@@ -1,45 +1,14 @@
-'use strict';
+'use strict'
 
-module.exports = function (server) {
-  var router = server.loopback.Router()
+module.exports = function (app) {
+  var router = app.loopback.Router()
   router.patch(
     "/activate/:codeid",
-    function (req, res) {
-      server.models.VoucherCode.findById(req.params.codeid, function (err, VoucherCode) {
-        if (err) res.json(err);
-        if(VoucherCode){
-          VoucherCode.updateAttribute("activated",true,function (uerr, uVoucherCode){
-            res.json(uVoucherCode);
-          })
-        }else {
-          var error = new Error();
-          error.message = 'Voucher Code not found.';
-          error.statusCode = 404;
-          res.json(error);
-        }
-      });
-    }
+    app.models.VoucherCode.activate
   );
   router.get(
     "/active/:codeid",
-    function (req, res) {
-      server.models.VoucherCode.findById(req.params.codeid,
-        function (err, vCode) {
-          if (err) {
-            console.log(err);
-            res.json(err);
-          } else {
-            if (vCode) {
-              res.json(vCode.activated);
-            } else {
-              var error = new Error();
-              error.message = 'Voucher Code not found.';
-              error.statusCode = 404;
-              res.json(error);
-            }
-          }
-        }
-      );
-    });
-  server.use(router);
+    app.models.VoucherCode.isActive
+    );
+  app.use(router);
 }
