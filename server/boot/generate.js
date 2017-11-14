7e9ac74b0e989e5ba4e632fcafedda1f3335f68f
as3mbus <asembus@gmail.com>
2017-11-14 14:00:58 +0700

    add body parser and generate request with dynamic outlet and expiredate

--- server/boot/generate.js
+++ server/boot/generate.js
@@ -1,13 +1,12 @@
 'use strict'
 
 module.exports = function (app) {
-    var router = app.loopback.Router()
-    router.post(
-      "/generate",
-      function (req, res) {
-        app.models.VoucherCode.generateCode(1, "2017-11-13T10:52:22.906Z", res);
-      }
-    );
-
+  var router = app.loopback.Router()
+  router.post(
+    "/generate",
+    function (req, res) {
+      app.models.VoucherCode.generateCode(req.body.outletId, req.body.expireDate, res);
+    }
+  );
   app.use(router);
 }
